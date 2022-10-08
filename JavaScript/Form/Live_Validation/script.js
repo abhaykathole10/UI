4803230cef20b94479b6aeb038b60cbf15f781c5
abhaykathole10 <abhaykathole10@gmail.com>
2022-10-08 12:27:02 +0530

    Form with live validation

---
+++ JavaScript/Form/Live_Validation/script.js
@@ -0,0 +1,15 @@
+function myFunc() {
+  var user_name = document.aForm.user_name
+  var err_msg = document.getElementById('err_msg')
+  if (user_name.value.length < 3) {
+    err_msg.innerHTML = 'Too small'
+    err_msg.style.color = 'red'
+    user_name.style.backgroundColor = 'aqua'
+  } else if (user_name.value.length == 3) {
+    err_msg.innerHTML = 'Perfect'
+    err_msg.style.color = 'green'
+  } else if (user_name.value.length > 7) {
+    err_msg.innerHTML = 'Too long'
+    err_msg.style.color = 'orange'
+  }
+}
