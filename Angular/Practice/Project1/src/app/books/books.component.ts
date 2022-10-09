a57fb85d53a50dbaf53bd782c0ca0b4b7c7ad09b
abhaykathole10 <abhaykathole10@gmail.com>
2022-10-09 14:07:26 +0530

    Interpolation in Angular

--- Angular/Practice/Project1/src/app/books/books.component.ts
+++ Angular/Practice/Project1/src/app/books/books.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
+  fname:string = "Abhay"
+  lname:string = "Kathole"
+
   constructor() { }
 
   ngOnInit(): void {
